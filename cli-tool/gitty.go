221179fd332099151b3e5e3f7713aba973640bdf
Saurabh Kumar <saurabh.k0402@gmail.com>
2021-01-22 23:06:30 +0530

    Store added repos to .gittystore

--- cli-tool/gitty.go
+++ cli-tool/gitty.go
@@ -1,57 +1,16 @@
 package main
 
 import (
+	"fileparser"
+	"finder"
 	"flag"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
-// findGitRepos recursively goes through all the folders within
-// the provided folder and finds folders that have git initialized.
-func findGitRepos(folder string) []string {
-	folder = strings.TrimSuffix(folder, "/")
-	gitRepos := []string{}
-
-	absPath, err := filepath.Abs(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	folder = absPath
-
-	fd, err := os.Open(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	files, err := fd.Readdir(-1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fd.Close()
-
-	for _, file := range files {
-		if file.IsDir() {
-			if file.Name() == ".git" {
-				gitRepos = append(gitRepos, folder)
-				continue
-			} else if file.Name() != "node_modules" {
-				moreRepos := findGitRepos(folder + "/" + file.Name())
-				gitRepos = append(gitRepos, moreRepos...)
-			}
-		}
-	}
-
-	return gitRepos
-}
-
 func scan(folder string) {
-	gitRepos := findGitRepos(folder)
-	for _, repo := range gitRepos {
-		print(repo + "\n")
-	}
+	gitRepos := finder.FindGitRepos(folder)
+	dotFilePath := finder.GetDotFilePath()
+	fileparser.AddNewReposToFile(gitRepos, dotFilePath)
 }
 
 func stats(email string) {}
--- cli-tool/go.mod
+++ cli-tool/go.mod
@@ -1,3 +1,12 @@
 module gitty
 
 go 1.14
+
+replace finder => ./finder
+
+replace fileparser => ./file-parser
+
+require (
+	fileparser v0.0.0-00010101000000-000000000000
+	finder v0.0.0-00010101000000-000000000000
+)
